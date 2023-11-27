6c7be40b2154a999cdd5525ac074286ec7f5bd6e
eryc_s.z.w <erycszw10@gmail.com>
2023-11-27 19:59:44 +0700

    add key feature to the encode and decode pages

--- src/pages/Decode.jsx
+++ src/pages/Decode.jsx
@@ -2,30 +2,58 @@ import { useState } from "react";
 import CryptoJS from 'crypto-js';
 
 const Decode = () => {
-    const [plainText,setPlainText] = useState('')
-    const [cipherText,setCipherText] = useState('')
+    const [element,setElement] = useState({
+        plainText: '',
+        cipherText:'',
+        key:''
+    })
+
+    function handleChange(e) {
+        const {name,value} = e.target
+        setElement(prevState=> {
+            return {
+                ...prevState,
+                [name] : value
+            }
+        })
+    }
+
+    function setPlainText(value) {
+        setElement(prevState=> {
+            return {
+                ...prevState,
+                plainText : value
+            }
+        })
+    }
     
-    const handleDecrypt = (e) => {
+    const handleEncrypt = (e) => {
         e.preventDefault()
-        const decrypted = CryptoJS.AES.decrypt(cipherText, 'secret-key').toString(CryptoJS.enc.Utf8);
+        const decrypted = CryptoJS.AES.decrypt(element.cipherText, element.key).toString(CryptoJS.enc.Utf8);
         setPlainText(decrypted);
     };
 
     return (
-        <div id="decode" className="font-poppins text-shine flex flex-col gap-10 h-[90vh] items-center justify-center text-xl">
+        <div id="decode" className="font-poppins text-shine flex flex-col gap-10 h-[100vh] items-center justify-center text-xl mt-20">
             <div className="title text-center">
                 <h2 className="text-4xl  font-bold text-accent">Decode</h2>
                 <p className="text-2xl">Decode your cipher text into plain text.</p>
             </div>
-            <form action="" className="flex gap-14" onSubmit={handleDecrypt}>
+            <form action="" className="flex-col flex gap-14" onSubmit={handleEncrypt}>
                 <div className="flex flex-col gap-5">
-                    <label htmlFor="">Insert your cipher text here : </label>
-                    <textarea name="cipherText" id="" cols="30" rows="10" className="input" value={cipherText} onChange={(e)=>setCipherText(e.target.value)}></textarea>
-                    <button className="btn-accent">Decrypt</button>
+                    <label htmlFor="key">input your key here : </label>
+                    <input type='text' name="key" id="key" className="input" value={element.key} onChange={handleChange}/>
                 </div>
-                <div className="flex flex-col gap-5">
-                    <label htmlFor="">Your plain text are : </label>
-                    <textarea name="plainText" id="" cols="30" rows="10" className="input" value={plainText}></textarea>
+                <div className="flex gap-14">
+                    <div className="flex flex-col gap-5">
+                        <label htmlFor="">Insert your cipher text here : </label>
+                        <textarea name="cipherText" id="" cols="30" rows="10" className="input" value={element.cipherText} onChange={handleChange}></textarea>
+                        <button className="btn-accent">Decrypt</button>
+                    </div>
+                    <div className="flex flex-col gap-5">
+                        <label htmlFor="">Your plain text are : </label>
+                        <textarea name="plainText" id="" cols="30" rows="10" className="input" value={element.plainText}></textarea>
+                    </div>
                 </div>
             </form>
         </div>
--- src/pages/Encode.jsx
+++ src/pages/Encode.jsx
@@ -2,12 +2,34 @@ import CryptoJS from 'crypto-js';
 import { useState } from 'react';
 
 const Encode = () => {
-    const [plainText,setPlainText] = useState('')
-    const [cipherText,setCipherText] = useState('')
-    
+    const [element,setElement] = useState({
+        plainText: '',
+        cipherText:'',
+        key:''
+    })
+
+    function handleChange(e) {
+        const {name,value} = e.target
+        setElement(prevState=> {
+            return {
+                ...prevState,
+                [name] : value
+            }
+        })
+    }
+
+    function setCipherText(value) {
+        setElement(prevState=> {
+            return {
+                ...prevState,
+                cipherText : value
+            }
+        })
+    }
+
     const handleEncrypt = (e) => {
         e.preventDefault()
-        const encrypted = CryptoJS.AES.encrypt(plainText, 'secret-key').toString();
+        const encrypted = CryptoJS.AES.encrypt(element.plainText, element.key).toString();
         setCipherText(encrypted);
     };
 
@@ -17,15 +39,21 @@ const Encode = () => {
                 <h2 className="text-4xl font-bold text-accent">Encode</h2>
                 <p className='text-2xl'>Encode your plain text into cipher text.</p>
             </div>
-            <form action="" className="flex gap-14" onSubmit={handleEncrypt}>
+            <form action="" className="flex-col flex gap-14" onSubmit={handleEncrypt}>
                 <div className="flex flex-col gap-5">
-                    <label htmlFor="">Insert your plain text here : </label>
-                    <textarea name="plainText" id="" cols="30" rows="10" className="input" value={plainText} onChange={(e)=>setPlainText(e.target.value)}></textarea>
-                    <button className="btn-accent">Encrypt</button>
+                    <label htmlFor="key">input your key here : </label>
+                    <input type='text' name="key" id="key" className="input" value={element.key} onChange={handleChange}/>
                 </div>
-                <div className="flex flex-col gap-5">
-                    <label htmlFor="">Your cipher text are : </label>
-                    <textarea name="cipherText" id="" cols="30" rows="10" className="input" value={cipherText}></textarea>
+                <div className="flex gap-14">
+                    <div className="flex flex-col gap-5">
+                        <label htmlFor="">Insert your plain text here : </label>
+                        <textarea name="plainText" id="" cols="30" rows="10" className="input" value={element.plainText} onChange={handleChange}></textarea>
+                        <button className="btn-accent">Encrypt</button>
+                    </div>
+                    <div className="flex flex-col gap-5">
+                        <label htmlFor="">Your cipher text are : </label>
+                        <textarea name="cipherText" id="" cols="30" rows="10" className="input" value={element.cipherText}></textarea>
+                    </div>
                 </div>
             </form>
         </div>
